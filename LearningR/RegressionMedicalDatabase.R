335971fd9cff45b68ee8bbdf0e96eef8b9bcd1ab
askofen <askofen.bsuir@hotmail.com>
2015-06-28 22:24:09 +0200

    Regression sample on medical database (R)

--- LearningR/RegressionMedicalDatabase.R
+++ LearningR/RegressionMedicalDatabase.R
@@ -1,8 +1,19 @@
 rm(list = ls())
 
+library(psych)
+
 insurance <- read.csv("Data/insurance.csv", stringsAsFactors = TRUE)
 
 print(str(insurance))
 print(summary(insurance$charges))
 
 hist(insurance$charges)
+
+#print the correlation matrix
+print(cor(insurance[c("age", "bmi", "children", "charges")]))
+
+#scatter plots matrix
+pairs(insurance[c("age", "bmi", "children", "charges")])
+
+#psych scatter plots matrix
+pairs.panels(insurance[c("age", "bmi", "children", "charges")])

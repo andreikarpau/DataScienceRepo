e710f954975478dcbf54270606d1e5e4237f5510
askofen <askofen.bsuir@hotmail.com>
2015-07-12 17:29:24 +0200

    Coursera learning R: assignment 1 Part 2

--- CourseraLearningR/Part1/Part1.R
+++ CourseraLearningR/Part1/Part1.R
@@ -1,9 +1,15 @@
 rm(list = ls())
 
-source('pollutantmean.R')
-print(pollutantmean('Data', "sulfate", id = 1:10))
-print(pollutantmean('Data', "nitrate", 70:72))
-print(pollutantmean('Data', "nitrate", 23))
+#source('pollutantmean.R')
+#print(pollutantmean('Data', "sulfate", id = 1:10))
+#print(pollutantmean('Data', "nitrate", 70:72))
+#print(pollutantmean('Data', "nitrate", 23))
+
+source('complete.R')
+print(complete('Data', 1))
+print(complete('Data', c(2, 4, 8, 10, 12)))
+print(complete('Data', 30:25))
+print(complete('Data', 3))
 
       
 
---
+++ CourseraLearningR/Part1/complete.R
@@ -0,0 +1,29 @@
+complete <- function(directory, id = 1:332) {
+  if(!file.exists(directory)) {
+    stop('directory does not exist')
+  }
+  
+  idsCount <- length(id)
+  dataFrame <- data.frame(id=numeric(idsCount), nobs=numeric(idsCount))
+
+  for (i in 1:idsCount){
+    fileName <- sprintf("%s/%s.csv", "Data", formatC(id[i], width=3, flag="0"))
+    table <- read.csv(fileName, stringsAsFactors = FALSE)
+    naSuldate <- is.na(table$sulfate)
+    naNitrate <- is.na(table$nitrate)
+    nonNaIDs <- table$ID[naSuldate == FALSE & naNitrate == FALSE]
+    dataFrame[i, ] <- list(id[i], length(nonNaIDs))
+  }
+  
+  dataFrame
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+}

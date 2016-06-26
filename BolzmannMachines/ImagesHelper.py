32e8bcff75997487bf3a472cf7e5683ad5c9e2bb
askofen <askofen.bsuir@hotmail.com>
2016-06-26 10:51:12 +0200

    Bolzman machine images sample

--- BolzmannMachines/ImagesHelper.py
+++ BolzmannMachines/ImagesHelper.py
@@ -15,7 +15,7 @@ class ImagesHelper:
 
         for i in range(0, image.size[0]):
             for j in range(0, image.size[1]):
-                pixel = pixels[i, j]
+                pixel = pixels[j, i]
 
                 if (PIXEL_COLOR_LIMIT < pixel[0] or PIXEL_COLOR_LIMIT < pixel[1] or PIXEL_COLOR_LIMIT < pixel[2]):
                     bitMask[i * image.size[1] + j] = 1
@@ -23,9 +23,12 @@ class ImagesHelper:
         return bitMask
 
     @staticmethod
-    def plot_bit_mask(bitMask, width, height):
-        image = np.asarray(bitMask).reshape(width, height)
-        plt.subplot(441)
-        plt.imshow(image)
+    def plot_bit_mask(bit_mask, width, height):
+        image = np.asarray(bit_mask).reshape(width, height)
+
+        figure = plt.figure()
+        subplot = figure.add_subplot(111)
+        subplot.imshow(image)
+        plt.show()
 
 

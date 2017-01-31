abbf2d4decc125f2c929b37e263cc02e91712ec0
askofen <askofen.bsuir@hotmail.com>
2017-01-31 22:03:28 +0100

    Roads parsing, province filter, sleep time are added into the script

--- TripAnalysisPrj/CarTrip.py
+++ TripAnalysisPrj/CarTrip.py
@@ -14,23 +14,34 @@ class Trip:
 
 class Car:
     def __init__(self, api_id, construction_year, engine_displacement, fuel_type, manufacturer, model):
-        self.__apiId = api_id
-        self.__constructionYear = construction_year
-        self.__engineDisplacement = engine_displacement
-        self.__fuelType = fuel_type
+        self.__api_id = api_id
+        self.__construction_year = construction_year
+        self.__engine_displacement = engine_displacement
+        self.__fuel_type = fuel_type
         self.__manufacturer = manufacturer
         self.__model = model
 
     def __str__(self):
-        return "Car {0}: {1} {2} {3} {4} {5}".format(self.__apiId, self.__manufacturer, self.__model,
-                                                     self.__engineDisplacement, self.__fuelType,
-                                                     self.__constructionYear)
+        return "Car {0}: {1} {2} {3} {4} {5}".format(self.__api_id, self.__manufacturer, self.__model,
+                                                     self.__engine_displacement, self.__fuel_type,
+                                                     self.__construction_year)
 
     def get_id(self):
-        return self.__apiId
+        return self.__api_id
 
 
 class Measurement:
     def __init__(self, latitude, longitude):
         self.__latitude = latitude
         self.__longitude = longitude
+
+
+class Road:
+    def __init__(self, display_name, osm_id, tags):
+        self.__display_name = display_name
+        self.__osm_id = osm_id
+        self.__tags = tags
+
+    def check_place(self, place_name):
+        dn = self.__display_name.lower()
+        return place_name.strip().lower() in dn
--- TripAnalysisPrj/TripsApiService.py
+++ TripAnalysisPrj/TripsApiService.py
@@ -1,10 +1,15 @@
 import CarTrip
 import requests
+import xml.etree.ElementTree as elementTree
+import time
 
 
 class CarService:
     def __init__(self):
         pass
+
+    __roads_id_cache = {}
+    __roads_coordinates_cache = {}
 
     @staticmethod
     def get_car_trips(page=1, limit_trips=100):
@@ -12,32 +17,45 @@ class CarService:
         tracks_data = response.json()
         tracks = tracks_data['tracks']
         trips = {}
+        check_index = 0;
 
         for value in tracks:
-            info = value['sensor']
+            try:
+                info = value['sensor']
 
-            if info['type'] != 'car':
-                continue
+                if info['type'] != 'car':
+                    continue
 
-            properties = info['properties']
-            car = CarTrip.Car(properties['id'], properties['constructionYear'], properties['engineDisplacement'],
-                              properties['fuelType'], properties['manufacturer'], properties['model'])
+                properties = info['properties']
+                car = CarTrip.Car(properties['id'], properties['constructionYear'], properties['engineDisplacement'],
+                                  properties['fuelType'], properties['manufacturer'], properties['model'])
 
-            trip_id = value['id']
-            measurements = CarService.get_car_measurements(trip_id)
-            trip = CarTrip.Trip(trip_id, value['length'], car, measurements)
+                trip_id = value['id']
 
-            trips[trip.get_id()] = trip
+                measurements = CarService.get_car_measurements(trip_id, "Mönchengladbach") #Mönchengladbach
+                if measurements is None:
+                    continue
+
+                tripLength = None if 'length' not in value else value['length']
+                trip = CarTrip.Trip(trip_id, tripLength, car, measurements)
+
+                trips[trip.get_id()] = trip
+            except Exception as e:
+                print('Exception in tracks on index = <{0}>. Exception: {1}'.format(check_index, e))
+
+            check_index += 1
 
         return trips
 
     @staticmethod
-    def get_car_measurements(tripId):
+    def get_car_measurements(trip_id, place):
         measurements = {}
         index = 0
 
-        response = requests.get('https://envirocar.org/api/stable/tracks/{0}/measurements'.format(tripId))
+        response = requests.get('https://envirocar.org/api/stable/tracks/{0}/measurements'.format(trip_id))
         features = response.json()['features']
+
+        place_checked = False
 
         for value in features:
             latitude = value['geometry']['coordinates'][1]
@@ -45,22 +63,49 @@ class CarService:
 
             road = CarService.get_road_info(latitude, longitude)
 
+            if not place_checked:
+                place_checked = True
+                if not road.check_place(place):
+                    return None
+
             measurements[index] = CarTrip.Measurement(value['geometry']['coordinates'][1],
                                                       value['geometry']['coordinates'][0])
             index += 1
 
         return measurements
 
-
     @staticmethod
     def get_road_info(latitude, longitude):
-        response = requests.get('http://nominatim.openstreetmap.org/reverse.php?format=json&lat={0}&lon={1}&zoom=16'.format(latitude, longitude))
+        lat_lot_hash = latitude + longitude;
+
+        if lat_lot_hash in CarService.__roads_coordinates_cache:
+            return CarService.__roads_coordinates_cache[lat_lot_hash]
+
+        time.sleep(1)
+
+        response = requests.get(
+            'http://nominatim.openstreetmap.org/reverse.php?format=json&lat={0}&lon={1}&zoom=16'.format(latitude,
+                                                                                                        longitude))
         road = response.json()
 
-        road_name = road['display_name']
         osm_id = road['osm_id']
 
-        # response_osm = requests.get('http://www.openstreetmap.org/api/0.6/way/{0}'.format(osm_id))
-        # road_info = response_osm.xml
+        if osm_id in CarService.__roads_id_cache:
+            return CarService.__roads_id_cache[osm_id]
 
-        return road_name
+        road_name = road['display_name']
+
+        response_osm = requests.get('http://www.openstreetmap.org/api/0.6/way/{0}'.format(osm_id))
+        way = elementTree.fromstring(response_osm.text).find("way")
+        tags = {}
+
+        if way is not None:
+            for tag in way.iter("tag"):
+                key = tag.attrib["k"]
+                value = tag.attrib["v"]
+                tags[key] = value
+
+        road = CarTrip.Road(road_name, osm_id, tags);
+        CarService.__roads_coordinates_cache[lat_lot_hash] = road
+        CarService.__roads_id_cache[osm_id] = road
+        return road

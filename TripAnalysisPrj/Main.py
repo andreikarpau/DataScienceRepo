b5b0d77bd647310516aab4222328e68223bd967c
Andrei Kaprau <Andrei Kaprau>
2017-02-03 16:15:59 +0100

    Additional trips data is serialized and saved

--- TripAnalysisPrj/Main.py
+++ TripAnalysisPrj/Main.py
@@ -3,12 +3,13 @@ import yaml
 
 
 def read_file(file_name):
-    with open(file_name, 'r') as myfile:
-        data = myfile.read().replace('\n', '')
-        trip = yaml.load(data)
-        print(trip)
+    with open(file_name, 'r') as trip_file:
+        data = trip_file.read().replace('\n', '')
+        loaded_trip = yaml.load(data)
+        print(loaded_trip)
 
-trips = envService.CarService.get_car_trips(2, 10)  # Range found: 1 - 70 for limit = 100
+# Ranges downloaded: 1,10;2,10;10,10;11,10;20,10;25,10;
+trips = envService.CarService.get_car_trips(25, 10)  # Range found: 1 - 70 for limit = 100
 
 print("{0}:".format(len(trips)))
 

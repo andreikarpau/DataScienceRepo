6ff9fccde94fa6aed069c87599e4082b384ef814
Andrei Kaprau <Andrei Kaprau>
2017-02-06 17:10:08 +0100

    Creating of the functionality for storing trips into csv

--- TripAnalysisPrj/CarTrip.py
+++ TripAnalysisPrj/CarTrip.py
@@ -8,13 +8,29 @@ class Trip:
     def get_id(self):
         return self.__id
 
+    def get_measurements(self):
+        return self.__measurements
+
     def __str__(self):
         return "Trip {0}, Length {1}; {2}".format(self.__id, self.__length, self.__car)
 
-    # def get_as_string(self):
-    #     final_str = ""
-    #     for measure in self.__measurements:
-    #         measure.
+    def get_simple_params(self):
+        return self.__id, self.__length
+
+    def save_to_array(self, measures_keys, tags_keys):
+        trip_array = [];
+        all_keys = ["trip_id", "trip_length", "car_id", "car_construction_year", "car_engine_displacement", "car_fuel_type",
+                    "car_manufacturer", "car_model"];
+
+        for measurement in self.get_measurements().values():
+            line = [];
+            line.extend(self.get_simple_params())
+            line.extend(self.__car.get_all_params())
+
+            trip_array.append(line)
+
+        return all_keys, trip_array
+
 
 class Car:
     def __init__(self, api_id, construction_year, engine_displacement, fuel_type, manufacturer, model):
@@ -33,6 +49,8 @@ class Car:
     def get_id(self):
         return self.__api_id
 
+    def get_all_params(self):
+        return self.__api_id, self.__construction_year, self.__engine_displacement, self.__fuel_type, self.__manufacturer, self.__model
 
 class Measurement:
     def __init__(self, index, latitude, longitude, road, odb2_measures):
@@ -46,14 +64,21 @@ class Measurement:
             self.__measures[key + "_value"] = value["value"]
             self.__measures[key + "_unit"] = value["unit"]
 
+    def get_measures(self):
+        return self.__measures
+
     def get_road(self):
         return self.__road;
+
 
 class Road:
     def __init__(self, display_name, osm_id, tags):
         self.__display_name = display_name
         self.__osm_id = osm_id
         self.__tags = tags
+
+    def get_tags(self):
+        return self.__tags
 
     def check_place(self, place_names):
         dn = self.__display_name.lower()
@@ -62,3 +87,20 @@ class Road:
                 return True
 
         return False
+
+
+class TripHelper:
+    @staticmethod
+    def get_all_measures_tags_names(trips):
+        measures = {}
+        tags = {}
+
+        for trip in trips:
+            for measurement in trip.get_measurements().values():
+                for measure_key in measurement.get_measures().keys():
+                    measures[measure_key] = "";
+
+                for tag_key in measurement.get_road().get_tags().keys():
+                    tags[tag_key] = "";
+
+        return measures, tags
--- TripAnalysisPrj/Main.py
+++ TripAnalysisPrj/Main.py
@@ -1,23 +1,48 @@
 import TripsApiService as envService
 import yaml
+import os
+from CarTrip import TripHelper
 
 
-def read_file(file_name):
+def read_trip_file(file_name):
     with open(file_name, 'r') as trip_file:
-        data = trip_file.read().replace('\n', '')
+        data = trip_file.read() #.replace('\n', '')
         loaded_trip = yaml.load(data)
-        print(loaded_trip)
+        return loaded_trip
 
-# Ranges downloaded: 1,10;2,10;10,10;11,10;20,10;25,10;
-trips = envService.CarService.get_car_trips(25, 10)  # Range found: 1 - 70 for limit = 100
+def download_and_save():
+    # Ranges downloaded: 1,10;2,10;10,10;11,10;20,10;25,10;
+    trips = envService.CarService.get_car_trips(25, 10)  # Range found: 1 - 70 for limit = 100
 
-print("{0}:".format(len(trips)))
+    print("{0}:".format(len(trips)))
 
-for trip in trips.values():
-    dumped_trip = yaml.dump(trip)
-    file = open('./data/{0}.trip'.format(trip.get_id()), 'w+')
-    file.write(dumped_trip)
-    file.close()
-    print(trip)
+    for trip in trips.values():
+        dumped_trip = yaml.dump(trip)
+        file = open('./data/{0}.trip'.format(trip.get_id()), 'w+')
+        file.write(dumped_trip)
+        file.close()
+        print(trip)
 
-# read_file('./data/588e364de4b04a0d732d7356.dat')
+    # read_file('./data/588e364de4b04a0d732d7356.dat')
+
+def read_all_trip_files():
+    entries = os.scandir('./data/')
+    trips = []
+
+    for f in entries:
+        if not f.is_file():
+            continue
+
+        trip = read_trip_file(f.path)
+        trips.append(trip)
+
+    return trips
+
+trips = read_all_trip_files()
+measures, tags = TripHelper.get_all_measures_tags_names(trips)
+
+for trip in trips:
+    keys, lines = trip.save_to_array(measures, tags)
+
+
+print(trips)

fe86fe74595d8d62a8856893c1116577afa041a8
Andrei Kaprau <Andrei Kaprau>
2017-03-15 16:59:35 +0100

    Trips analysis

--- TripAnalysisPrj/RTripsAnalysis/preprocess.R
+++ TripAnalysisPrj/RTripsAnalysis/preprocess.R
@@ -15,17 +15,17 @@ plot_co2_points <- function(car_data) {
 }
 
 csv_files = list.files(path = "./csv/", pattern="*.csv", full.names=TRUE)
-car_trips = create_car_trips_dataframe(csv_files[8])
+car_trips = create_car_trips_dataframe(csv_files[2])
 
 
-car_data = car_trips[car_trips$trip_id=="5850443ae4b0a979d5501cb3",]
+car_data = car_trips[car_trips$trip_id=="58716c9ae4b04a0d71912dd5",]
 plot_co2_points(car_trips)
 
 plot_speed_co2 <- function(car_data) {
   ggplot(data=car_data, aes(x=time)) + geom_line(aes(y=Speed_value, colour="Speed")) +
     geom_line(aes(y=CO2_value, colour="Emission CO2")) + 
     geom_point(aes(y=Speed_value, colour=highway)) +
-    geom_line(aes(y=Intake.Pressure_value, colour="Temp"))
+    geom_line(aes(y=Calculated.MAF_value, colour="Temp"))
 }
 
 plot_speed_co2(car_data)

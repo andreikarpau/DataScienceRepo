ca6cd47cf7565d5fc74ddc8bb23d492f5ccad985
askofen <askofen.bsuir@hotmail.com>
2017-09-20 21:51:59 +0200

    Changes in CNN

--- CnnMNIST/main.py
+++ CnnMNIST/main.py
@@ -1,6 +1,35 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+
+def create_new_conv_layer(input_data, num_input_channels, num_filters,
+                          filter_shape, pool_shape, name):
+
+    conv_filt_shape = [filter_shape[0], filter_shape[1], num_input_channels, num_filters]
+
+    # initialise weights and bias for the filter
+    weights = tf.Variable(tf.truncated_normal(conv_filt_shape, stddev=0.03), name=name + '_W')
+    bias = tf.Variable(tf.truncated_normal([num_filters]), name=name + '_b')
+
+    # setup the convolutional layer operation
+    out_layer = tf.nn.conv2d(input_data, weights, [1, 1, 1, 1], padding='SAME')
+
+    # add the bias
+    out_layer += bias
+
+    # apply a ReLU non-linear activation
+    out_layer = tf.nn.relu(out_layer)
+
+    # now perform max pooling
+    ksize = [1, 2, 2, 1]
+    strides = [1, 2, 2, 1]
+    out_layer = tf.nn.max_pool(out_layer, ksize=ksize, strides=strides,
+                               padding='SAME')
+
+    return out_layer
+
+
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 learning_rate = 0.0001
@@ -13,6 +42,9 @@ x_shaped = tf.reshape(x, [-1, 28, 28, 1])
 
 # output
 y = tf.placeholder(tf.float32, [None, 10])
+
+layer1 = create_new_conv_layer(x_shaped, 1, 32, [5, 5], [2, 2], name='layer1')
+layer2 = create_new_conv_layer(layer1, 32, 64, [5, 5], [2, 2], name='layer2')
 
 init_op = tf.global_variables_initializer()
 

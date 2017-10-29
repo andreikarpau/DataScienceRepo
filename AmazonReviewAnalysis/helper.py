bc415f0460f0b6e8ae59afbef9a8dc0dfa6e0488
askofen <askofen.bsuir@hotmail.com>
2017-10-29 15:40:30 +0100

    Amazon review analysis process

--- AmazonReviewAnalysis/helper.py
+++ AmazonReviewAnalysis/helper.py
@@ -1,18 +1,32 @@
 import json
 
+import numpy as np
+
 
 class FileHelper:
     @staticmethod
-    def read_data_file(name):
+    def read_data_file(name, max_num=None):
         texts = []
         summaries = []
         overall = []
 
+        iteration = 0
         with open(name, "r") as json_file:
             for line in json_file:
+                iteration = iteration + 1
+                if max_num is not None and max_num <= iteration:
+                    break
+
                 str = json.loads(line)
                 texts.append(str["reviewText"])
                 summaries.append(str["summary"])
                 overall.append(str["overall"])
 
         return texts, summaries, overall
+
+    @staticmethod
+    def read_word2vec(file_name="word2vec/glove.6B.50d.txt"):
+        with open(file_name, "rb") as lines:
+            w2v = {line.split()[0]: np.array(map(float, line.split()[1:]))
+                   for line in lines}
+            return w2v
--- AmazonReviewAnalysis/main.py
+++ AmazonReviewAnalysis/main.py
@@ -1,11 +1,64 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+
+from MeanEmbeddingVectorizer import MeanEmbeddingVectorizer
 from helper import FileHelper
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, HashingVectorizer, TfidfVectorizer
+from sklearn.model_selection import GroupShuffleSplit, StratifiedShuffleSplit
 
-texts, _, rates = FileHelper.read_data_file("data/Short_Data.json")
+#texts, _, rates = FileHelper.read_data_file("data/Short_Data.json")
+texts, _, rates = FileHelper.read_data_file("data/Grocery_Gourmet_Food.json", max_num=1000)
 
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(texts)
+rates = [3 < val for val in rates]
 
-vocabulary = vectorizer.vocabulary_
+vectorizer = CountVectorizer(stop_words='english')
+#vectorizer = HashingVectorizer(stop_words='english', alternate_sign=False)
+#vectorizer = TfidfVectorizer(sublinear_tf=True, stop_words='english')
+#vectorizer = MeanEmbeddingVectorizer(FileHelper.read_word2vec())
 
-print(X)
+#classifier = MultinomialNB()
+#classifier = OneVsRestClassifier(LinearSVC())
+classifier = RandomForestClassifier(n_estimators=10)
+#classifier = OneVsRestClassifier(LogisticRegression())
+#classifier = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+
+
+k_folds = StratifiedShuffleSplit(n_splits=5, test_size=0.3, random_state=345)
+
+accuracy = []
+prf_array = []
+
+iteration = 0
+
+for train_index, test_index in k_folds.split(texts, rates):
+    iteration = iteration + 1
+
+    x_train = [texts[i] for i in train_index]
+    y_train = [rates[i] for i in train_index]
+
+    x_test = [texts[i] for i in test_index]
+    y_test = [rates[i] for i in test_index]
+
+    x_train_vectorized = vectorizer.fit_transform(x_train)
+    clf = classifier.fit(x_train_vectorized, y_train)
+
+    x_test_vectorized = vectorizer.transform(x_test)
+    predicted = clf.predict(x_test_vectorized)
+
+    a_score = accuracy_score(y_pred=predicted, y_true=y_test)
+    prf = precision_recall_fscore_support(y_pred=predicted, y_true=y_test, average='binary')
+
+    accuracy.append(a_score)
+    prf_array.append(prf)
+    
+    print("Accuracy iteration {0}: {1}; Precision, recall, F score: {2}".format(iteration, a_score, prf))
+    print(a_score)
+
+total_accuracy = sum(accuracy) / len(accuracy)
+print("Accuracy: ")
+print(total_accuracy)
